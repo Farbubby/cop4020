f45184511b1577d90e3dd2d1a9980f84645464eb
farbubby <farhan.mahbub@ucf.edu>
2024-01-17 20:42:57 -0500

    solved question 2 in second assignment and fixed comments

--- firstAssignment.hs
+++ firstAssignment.hs
@@ -1,31 +1,31 @@
 -- Farhan Mahbub
 -- COP4020 Spring 2024
 
---1
+-- 1
 multi2 :: Float -> Float -> Float
 multi2 x y = x * y
 
---2
+-- 2
 multi3 :: Float -> Float -> Float -> Float
 multi3 x y z = x * y * z
 
---3
+-- 3
 first_a :: Int -> [Int]
 first_a n = [x | x <- [1..n], x `mod` 6 == 0 || x `mod` 11 == 0]
 
---4
-isMult6Or11 :: Int -> Bool --Helper function
+-- 4
+isMult6Or11 :: Int -> Bool -- Helper function
 isMult6Or11 x = x `mod` 6 == 0 || x `mod` 11 == 0
 
 first_b :: Int -> [Int]
 first_b n = [x | x <- [1..n], isMult6Or11 x]
 
---5
+-- 5
 second_a :: Int -> [Int]
 second_a n = [x | x <- [1..n], x `mod` 10 == 3, let str = show x in str == reverse str]
 
---6
-isPalindromeThatStartsWithDigit3 :: Int -> Bool --Helper function
+-- 6
+isPalindromeThatStartsWithDigit3 :: Int -> Bool -- Helper function
 isPalindromeThatStartsWithDigit3 x = x `mod` 10 == 3 && let str = show x in str == reverse str
 
 second_b :: Int -> [Int]
--- secondAssignment.hs
+++ secondAssignment.hs
@@ -1,15 +1,33 @@
 -- Farhan Mahbub
 -- COP4020 Spring 2024
 
---1
-newSqrt :: Float -> Int
+-- 1
+newSqrt :: Float -> Int -- Helper function
 newSqrt n = floor (sqrt (n))
 
-isPrime :: Int -> Bool
+isPrime :: Int -> Bool -- Helper function
 isPrime n = length [x | x <- [2..newSqrt (fromIntegral n)], n `mod` x == 0] == 0
 
-primesList :: Int -> [Int]
+primesList :: Int -> [Int] -- Helper function
 primesList n = take n [x | x <- [2..], isPrime x]
 
 problem1 :: Int -> [Int]
 problem1 n = [let list = primesList (n*2) in list !! x | x <- [0..(n*2 - 1)], x `mod` 2 == 0]
+
+-- 2
+fib :: Int -> Int -- Helper function
+fib 0 = 0
+fib 1 = 1
+fib n = fib (n-1) + fib (n-2)
+
+fiboThree :: Int -> Int -> [Int] -- Helper function
+fiboThree x n = let num = fib x in 
+    if (num > n) 
+        then [] 
+    else
+        if (num `mod` 10 == 3) 
+            then [num] ++ fiboThree (x+1) n 
+        else fiboThree (x+1) n
+
+problem2 :: Int -> [Int]
+problem2 n = fiboThree 2 n
