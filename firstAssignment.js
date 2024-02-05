9c8b9dd6e80f92cbbe5e61e4fc652610e240b966
farbubby <farhan.mahbub@ucf.edu>
2024-02-05 13:48:03 -0500

    completed first problem of first js assignment

---
+++ firstAssignment.js
@@ -0,0 +1,37 @@
+// Farhan Mahbub
+// COP4020 Spring 2024
+
+// Problem 1
+function collatzSequenceLength(start) {
+  sequence = [];
+
+  while (start != 1) {
+    sequence.push(start);
+    if (start % 2 == 0) {
+      start = start / 2;
+    } else {
+      start = 3 * start + 1;
+    }
+  }
+
+  sequence.push(1);
+
+  return sequence.length;
+}
+
+function longestChain(limit) {
+  let longest = 0;
+  let number = 0;
+
+  for (let i = 1; i < limit; i++) {
+    let length = collatzSequenceLength(i);
+    if (length > longest) {
+      longest = length;
+      number = i;
+    }
+  }
+
+  return number;
+}
+
+console.log("Problem 1: " + longestChain(1000000)); // 837799
