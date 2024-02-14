e9cceefa2f30361c08f45be919f17b772b3be239
farbubby <farhan.mahbub@ucf.edu>
2024-02-14 12:09:55 -0500

    solved problem 1 and 2 and changed tsconfig file

--- TypeScript/firstAssignment.ts
+++ TypeScript/firstAssignment.ts
@@ -3,8 +3,41 @@
 import { readFileSync } from "fs";
 
 // Problem 1
+function lexicoPerm(n: number) {
+  let str = "0123456789";
+  let result = "";
+  let fact = 1;
+
+  for (let i = 1; i <= 10; i++) {
+    fact *= i;
+  }
+
+  for (let i = 0; i < 10; i++) {
+    fact /= 10 - i;
+    let index = Math.floor(n / fact);
+    result += str.charAt(index);
+    str = str.slice(0, index) + str.slice(index + 1);
+    n -= index * fact;
+  }
+
+  return result;
+}
 
 // Problem 2
+function coinSum(target: number, coins: number[]) {
+  const dp: number[] = new Array(target + 1).fill(0);
+  dp[0] = 1;
+
+  for (let coin of coins) {
+    for (let i = coin; i <= target; i++) {
+      dp[i] += dp[i - coin];
+    }
+  }
+
+  return dp[target];
+}
+
+console.log("Problem 2: " + coinSum(200, [1, 2, 5, 10, 20, 50, 100, 200]));
 
 // Problem 3
 function isTriangleNumber(x: number) {
@@ -39,4 +72,4 @@ function countTriangleWords(filename: string) {
   return text.split(",").filter((str: string) => isTriangleWord(str)).length;
 }
 
-console.log(countTriangleWords("./0042_words.txt"));
+console.log("Problem 3:" + countTriangleWords("./0042_words.txt"));
