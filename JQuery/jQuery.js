a6754419d4d97b39fe5698fdfba9a57cdcb32e53
farbubby <farhan.mahbub@ucf.edu>
2024-02-25 13:18:29 -0500

    added feature to shuffle puzzle pieces and refactored code

--- JQuery/jQuery.js
+++ JQuery/jQuery.js
@@ -1,3 +1,13 @@
 $(function () {
-  $("img#draggable").draggable();
+  $(".draggable").draggable();
+
+  const pieces = $(".shuffle");
+  pieces.each((i, piece) => {
+    const randomX =
+      Math.floor(Math.random() * 180) * (Math.random() < 0.5 ? -1 : 1);
+    const randomY =
+      Math.floor(Math.random() * 180) * (Math.random() < 0.5 ? -1 : 1);
+    piece.style.transform = `translate(${randomX}px, ${randomY}px)`;
+    piece.style.transition = "transform 1s";
+  });
 });
