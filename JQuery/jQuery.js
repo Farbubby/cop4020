8079a1de3441625a4f79c03f89bb46e6c5bc0786
farbubby <farhan.mahbub@ucf.edu>
2024-02-25 15:41:35 -0500

    have all of the pieces fall into their final solution position except one

--- JQuery/jQuery.js
+++ JQuery/jQuery.js
@@ -8,6 +8,14 @@ $(function () {
     const randomY =
       Math.floor(Math.random() * 180) * (Math.random() < 0.5 ? -1 : 1);
     piece.style.transform = `translate(${randomX}px, ${randomY}px)`;
-    piece.style.transition = "transform 1s";
+    piece.style.transition = "transform 1s ease";
   });
+
+  setTimeout(() => {
+    pieces.each((i, piece) => {
+      piece.style.transform = "";
+      piece.style.transition = "";
+      piece.classList.add(`transition-${i + 1}`);
+    });
+  }, 1500);
 });
