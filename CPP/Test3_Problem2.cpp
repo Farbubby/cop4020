cb201bfdd571a24a9f27d2f67c0c9b2e8170ceb2
farbubby <farhan.mahbub@ucf.edu>
2024-03-28 17:01:35 -0400

    Completed problem 2 of Test 3

---
+++ CPP/Test3_Problem2.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <thread>
+#include <mutex>
+
+using namespace std;
+
+mutex mtx;
+
+void mt1() {
+    for (int i = 1; i <= 10000; ++i) {
+        mtx.lock();
+        cout << "1:" << i << endl;
+        mtx.unlock();
+    }
+}
+
+void mt2() {
+    for (int i = 2; i <= 20000; i += 2) {
+        mtx.lock();
+        cout << "2:" << i << endl;
+        mtx.unlock();
+    }
+}
+
+void mt3() {
+    for (int i = 7; i <= 70000; i += 7) {
+        mtx.lock();
+        cout << "3:" << i << endl;
+        mtx.unlock();
+    }
+}
+
+int main() {
+    thread thread1(mt1);
+    thread thread2(mt2);
+    thread thread3(mt3);
+
+    thread1.join();
+    thread2.join();
+    thread3.join();
+
+    return 0;
+}
